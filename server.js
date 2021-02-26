77564919abf731b1916bf855245d53f96e2829b2
Francesco <vaubanf@gmail.com>
2021-02-26 15:45:22 +0100

    edamam api test ok, looking into spoonacular

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const path = require("path");
 const morgan = require("morgan");
 const compression = require("compression");
 const bodyParser = require("body-parser");
+const https = require("https");
+const secrets = require("./secrets.json");
 
 const app = express();
 
@@ -14,9 +16,36 @@ app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/api/test', (req, res) => {
-    console.log('receiveedddddd');
+app.get("/api/test", (req, res) => {
+    console.log("receiveedddddd");
     res.json("cool");
+});
+
+app.post("/api/submitIngredientsList", async (req, res) => {
+    console.log("req.body: ", req.body);
+    let queryString = req.body.join("+");
+    console.log(queryString);
+
+    https.get(
+        `https://api.edamam.com/search?q=${queryString}&app_id=${secrets.app_id}&app_key=${secrets.app_key}&from=0&to=25`,
+        (res) => {
+            if (res.status === "error") {
+                console.log("error message: ", res.message);
+                return;
+            }
+
+            let body = "";
+
+            res.on("data", (chunk) => {
+                body += chunk;
+            });
+
+            res.on("end", () => {
+                let response = JSON.parse(body);
+                console.log("response: ", response.hits);
+            });
+        }
+    );
 });
 
 app.listen(process.env.PORT || 3001, () => {
--- src/homeSearch.js
+++ src/homeSearch.js
@@ -1,6 +1,15 @@
 import axios from "axios";
+import {useState} from "react";
 
 const HomeSearch = () => {
+    const [ingredient, setIngredient] = useState("");
+    const [ingredientsList, setIngredientsList] = useState([]);
+
+    console.log('arr: ', ingredientsList);
+
+    let submitList = async () => {
+        axios.post("/api/submitIngredientsList", ingredientsList);
+    };
 
     let fn = async () => {
         let res = await axios.get("/api/test");
@@ -8,9 +17,38 @@ const HomeSearch = () => {
     };
 
     return (
-        <div>
+        <div className="p-2">
             <h1 className="text-6xl">Hello Francesco</h1>
-            <button onClick={() => fn()}>Click me</button>
+            <button onClick={() => fn()}>Click me to test server</button>
+            <div>
+                <input
+                    onKeyPress={(e) => { 
+                        if (e.key === 'Enter'){
+                            ingredientsList.push(ingredient);
+                            setIngredient("");
+                        }
+                    }}
+                    onChange={(e) => setIngredient(e.target.value)}
+                    type="text"
+                    placeholder="enter one ingredient"
+                    value={ingredient}
+                />
+                <button
+                    onClick={() => {
+                        ingredientsList.push(ingredient);
+                        setIngredient("");
+                    }}
+                    className="border-solid border-black border-2 rounded-lg p-1"
+                >
+                    Add the ingredient to the list
+                </button>
+                <button
+                    className="border-solid border-black border-2 rounded-lg p-1"
+                    onClick={() => submitList()}
+                    >
+                    Submit the List
+                </button>
+            </div>
         </div>
     );
 }
